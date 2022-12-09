98c98ac3aa8b3382563844efb2064ecf02ee62f7
Elspeth Brown <elspethrbrown@outlook.com>
2022-12-09 11:46:29 +0000

    finish setting up

--- Models/models.js
+++ Models/models.js
@@ -1,1 +1,7 @@
-import { query } from '../db/index.js';
+import { query } from "../db/index.js";
+
+//GET all favourite movies
+
+//POST new favourite movie
+
+//DELETE favourite movie
--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
 
 export const app = express();
-import routerName from './Routes/routes.js';
+import router from "./Routes/routes.js";
 
-app.use(cors('*'));
-app.use(morgan('dev'));
-app.use(express.static('public'));
+app.use(cors("*"));
+app.use(morgan("dev"));
 app.use(express.json());
 
-app.use('/', routerName);
+app.use("/favourites", router);
